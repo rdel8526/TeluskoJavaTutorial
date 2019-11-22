17197540b492205efc52440cd370c2393d70e935
rdel8526 <47870554+rdel8526@users.noreply.github.com>
2019-11-22 21:11:42 +1100

    Update LoopAssignments.java
    Completed all 3 Loop Assignments

--- LoopAssignments.java
+++ LoopAssignments.java
@@ -13,4 +13,15 @@ public class LoopAssignments {
                 System.out.print(ch + " ");
             } System.out.println();
 	
+	for(int i = 1; i <= 4; i++) {
+            for(int j = 1; j <= 4; j ++) {
+                if(i == 1 || i == 4 || j == 1 || j == 4) { 
+                    System.out.print("*");
+                }
+                else {   
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
 }
